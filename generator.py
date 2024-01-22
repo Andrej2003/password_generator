a92cd0a05969d9982b682795a05fe4ddb138185b
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2024-01-22 09:54:08 +0100

    refactor

--- generator.py
+++ generator.py
@@ -1,11 +1,10 @@
 import random
 import string
 
-pswd = ""
-gen = random.sample(string.ascii_letters + string.digits + string.punctuation, k=16)
+gen = random.sample(string.ascii_letters +
+                    string.digits + string.punctuation, k=16)
 
-pswd = pswd.join(gen)
+pswd = "".join(gen)
 
 if __name__ == "__main__":
     print(pswd)
-

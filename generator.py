353e0db8d248dadea5a229e6072d0520aeda4e63
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2024-01-24 10:04:53 +0100

    refactor into function

--- generator.py
+++ generator.py
@@ -1,10 +1,15 @@
 import random
 import string
 
-gen = random.sample(string.ascii_letters +
-                    string.digits + string.punctuation, k=16)
 
-pswd = "".join(gen)
+def generate():
+    gen = random.sample(string.ascii_letters +
+                        string.digits + string.punctuation, k=16)
+
+    pswd = "".join(gen)
+
+    return pswd
+
 
 if __name__ == "__main__":
-    print(pswd)
+    print(generate())

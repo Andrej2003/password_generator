94072892555fa6347fbac15d257d6f16c2d50506
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2024-01-25 08:39:01 +0100

    fix copy button bug

--- gui_gen.py
+++ gui_gen.py
@@ -5,17 +5,22 @@ from generator import generate
 
 def regenerate_pswd():
     label_pswd['text'] = generate()
-    copy_button["command"] = root.clipboard_clear()
-    copy_button['command'] = root.clipboard_append(label_pswd['text'])
+
 
     return label_pswd["text"]
+
+def copy_pswd():
+    copy_button["command"] = root.clipboard_clear()
+    copy_button['command'] = root.clipboard_append(label_pswd['text'])
 
 
 root = Tk()
 root.title("Password Generator")
 root.iconbitmap("8726250_padlock_icon.ico")
 
-frm = ttk.Frame(root, padding=10, border=150)
+frm_style = ttk.Style()
+# frm_style.configure("TFrame", background="#d8b3fc")
+frm = ttk.Frame(root, padding=10, border=250, style="TFrame")
 frm.grid()
 
 label_style = ttk.Style()
@@ -25,8 +30,7 @@ label_pswd.grid(column=0, row=0)
 
 button_style = ttk.Style()
 button_style.configure("W.TButton", font=("arial", 20))
-copy_button = ttk.Button(frm, text="Copy", command=root.clipboard_append(
-    label_pswd['text']), style="W.TButton")
+copy_button = ttk.Button(frm, text="Copy", command=copy_pswd, style="W.TButton")
 copy_button.grid(column=0, row=1)
 ttk.Button(frm, text="Regenerate", command=regenerate_pswd,
            style="W.TButton").grid(column=0, row=2)

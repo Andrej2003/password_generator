452386bf4475e5e732cab304d1678a353339cb61
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2024-01-27 10:55:14 +0100

    change button style

--- gui_gen.py
+++ gui_gen.py
@@ -18,20 +18,21 @@ root = Tk()
 root.title("Password Generator")
 root.iconbitmap("8726250_padlock_icon.ico")
 
-frm_style = ttk.Style()
-frm_style.configure("TFrame", background="#d8b3fc")
+style = ttk.Style()
+style.theme_use("default")
+style.configure("TFrame", background="#d8b3fc")
+style.configure("W.TLabel", font=("arial", 20))
+style.configure("W.TButton", font=("arial", 20),
+                foreground="black", background="white", relief="flat", borderwidth=0)
 frm = ttk.Frame(root, padding=10, border=250, style="TFrame")
 frm.grid()
 
-label_style = ttk.Style()
-label_style.configure("W.TLabel", font=("arial", 20))
 label_pswd = ttk.Label(
     frm, text=f"{generate()}", style="W.TLabel", background="#d8b3fc")
 label_pswd.grid(column=0, row=0, pady=7)
 
-button_style = ttk.Style()
-button_style.configure("W.TButton", font=("arial", 20))
-copy_button = ttk.Button(frm, text="Copy", command=copy_pswd, style="W.TButton")
+copy_button = ttk.Button(
+    frm, text="Copy", command=copy_pswd, style="W.TButton")
 copy_button.grid(column=0, row=1, pady=3)
 ttk.Button(frm, text="Regenerate", command=regenerate_pswd,
            style="W.TButton").grid(column=0, row=2, pady=3)
